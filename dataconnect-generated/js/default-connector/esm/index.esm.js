e5696ec5deed3e009cb1ec052a013018047f3d79
Abhishek2586 <76675704+Abhishek2586@users.noreply.github.com>
2025-06-14 01:21:37 +0530

    0121

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'nexora',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'nexora',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
